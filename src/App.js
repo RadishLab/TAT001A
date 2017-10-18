8f15ad9962412b8ca77c6a789626826373bdb2e5
Eric Brelsford <ebrelsford@gmail.com>
2017-10-18 14:51:18 -0400

    Page name, structure

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import Map1 from './chapters/ch1/Map1';
 class App extends Component {
   componentDidMount() {
     new Map1(this.svgRootMap, 1000, 600);
-    this.lineSmall = new Chart1(this.svgRootLineSmall, 340, 178);
-    this.lineSmaller = new Chart1(this.svgRootLineSmaller, 188, 158);
+    new Chart1(this.svgRootLineSmall, 340, 178);
+    new Chart1(this.svgRootLineSmaller, 188, 158);
   }
 
   render() {
     return (
-      <div className="app">
-        <svg id='1-map' ref={(svg) => { this.svgRootMap = svg; }}></svg>
-        <svg id='1-inset1-wide' ref={(svg) => { this.svgRootLineSmall = svg; }}></svg>
-        <svg id='1-inset1-narrow' ref={(svg) => { this.svgRootLineSmaller = svg; }}></svg>
+      <div>
+        <section>
+          <h1>Chapter 1</h1>
+          <svg id='1-map' ref={(svg) => { this.svgRootMap = svg; }}></svg>
+          <svg id='1-inset1-wide' ref={(svg) => { this.svgRootLineSmall = svg; }}></svg>
+          <svg id='1-inset1-narrow' ref={(svg) => { this.svgRootLineSmaller = svg; }}></svg>
+        </section>
       </div>
     );
   }
