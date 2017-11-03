ff27b1e6e7c40273ab7d8143360344221721c5cc
Eric Brelsford <ebrelsford@gmail.com>
2017-11-03 15:00:52 -0400

    Chapter 8 map

--- src/chapters/ch8/Figures.js
+++ src/chapters/ch8/Figures.js
@@ -2,8 +2,14 @@ import { Chart1 } from './Chart1';
 import { Chart2 } from './Chart2';
 import { Chart3 } from './Chart3';
 import { Chart4 } from './Chart4';
+import Map from './Map';
 
 export const figures = [
+  {
+    name: 'map',
+    figureClass: Map,
+    type: 'map'
+  },
   {
     name: 'inset1',
     figureClass: Chart1,
---
+++ src/chapters/ch8/Map.js
@@ -0,0 +1,33 @@
+import { csv } from 'd3-request';
+import { scaleOrdinal } from 'd3-scale';
+
+import { schemeCategoryProblemMap } from '../../colors';
+import { dataUrl } from '../../dataService';
+import WorldMap from '../../maps/WorldMap';
+
+export default class Map extends WorldMap {
+  constructor(parent, width, height) {
+    super(parent, width, height);
+    this.colorScale = scaleOrdinal(schemeCategoryProblemMap);
+    this.valueField = 'KeyCode';
+    this.symbolField = 'Symbol (Female ≥15%)';
+  }
+
+  loadJoinData() {
+    return new Promise((resolve, reject) => {
+      csv(dataUrl('8-map.csv'), (csvData) => {
+        resolve(csvData);
+      });
+    });
+  }
+
+  join(countries, joinData) {
+    countries.features.forEach(feature => {
+      const countryData = joinData.filter(row => row.ISO3 === feature.properties.iso_a3);
+      if (countryData.length > 0) {
+        feature.properties.joined = countryData[0];
+      }
+    });
+    return countries;
+  }
+}
