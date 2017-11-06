9a0c67ca0d8abc10fdb431969f5066bce385426b
Eric Brelsford <ebrelsford@gmail.com>
2017-11-06 17:55:14 -0500

    Add chapter 5 data

--- scripts/gatherdata.js
+++ scripts/gatherdata.js
@@ -20,15 +20,18 @@ const FILES = {
   '4 Prevalence/TA6 Prevalence Inset 3-Monitoring.xlsx': '4-3.csv',
   '4 Prevalence/TA6 Prevalence Inset 4-Consumption.xlsx': '4-4.csv',
   '4 Prevalence/TA6 Prevalence Map & Symbol.xlsx': '4-map.csv',
+  '5 Secondhand/TA6 Secondhand Inset 2-SHS in select countries.xlsx': '5-2.csv',
+  '5 Secondhand/TA6 Secondhand Inset 3-SHS in Europe.xlsx': '5-3.csv',
+  '5 Secondhand/TA6 Secondhand Inset 4-SHS by socioeconomic status.xlsx': '5-4.csv',
   '8 Deaths/TA6 Deaths Inset 1-Death by WHO region.xlsx': '8-1.csv',
   '8 Deaths/TA6 Deaths Inset 2-Cancer deaths in USA.xlsx': '8-2.csv',
   '8 Deaths/TA6 Deaths Inset 3-Attributable Deaths by Region.xlsx': '8-3.csv',
   '8 Deaths/TA6 Deaths Inset 4-Lung cancer & SES.xlsx': '8-4.csv',
   '8 Deaths/TA6 Deaths Map & Symbol.xlsx': '8-map.csv',
-  '9 Societal/TA6 Societal Inset1.xlsx': '9-1.csv',
-  '9 Societal/TA6 Societal Inset2 Jamaica.xlsx': '9-2.csv',
-  '9 Societal/TA6 Societal Inset3.xlsx': '9-3.csv',
-  '9 Societal/TA6 Societal Map.xlsx': '9-map.csv',
+  '9 Societal Harms/TA6 Societal Inset1.xlsx': '9-1.csv',
+  '9 Societal Harms/TA6 Societal Inset2 Jamaica.xlsx': '9-2.csv',
+  '9 Societal Harms/TA6 Societal Inset3.xlsx': '9-3.csv',
+  '9 Societal Harms/TA6 Societal Map.xlsx': '9-map.csv',
 };
 
 const execPromise = promisify(exec);
