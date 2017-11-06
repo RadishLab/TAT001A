80c59941109fb548e0e60b1aefb0c7923183e0e9
Eric Brelsford <ebrelsford@gmail.com>
2017-11-06 18:13:15 -0500

    Add chapter 16 data

--- scripts/gatherdata.js
+++ scripts/gatherdata.js
@@ -46,6 +46,7 @@ const FILES = {
   '13 Smokefree/High achieving smoke-free countries.xlsx': '13-map.csv',
   '13 Smokefree/Smokefree inset 1.xlsx': '13-1.csv',
   '15 Partnerships/TA6 Map_Partnerships.xlsx': '15-map.csv',
+  '16 ENDS/TA6 ENDS Map (Market Access).xlsx': '16-map.csv',
 };
 
 const execPromise = promisify(exec);
