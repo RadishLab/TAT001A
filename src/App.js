605f288399236a26cfb689f61d64ed32871de9d8
Eric Brelsford <ebrelsford@gmail.com>
2017-11-06 23:05:39 -0500

    Chapter 2, inset 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import FigureGroup from './FigureGroup';
 import { figures as ch1Figures } from './chapters/ch1/Figures';
+import { figures as ch2Figures } from './chapters/ch2/Figures';
 import { figures as ch4Figures } from './chapters/ch4/Figures';
 import { figures as ch8Figures } from './chapters/ch8/Figures';
 import { figures as ch9Figures } from './chapters/ch9/Figures';
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div>
         <FigureGroup title='Chapter 1' chapter={1} figures={ch1Figures}/>
+        <FigureGroup title='Chapter 2' chapter={2} figures={ch2Figures}/>
         <FigureGroup title='Chapter 4' chapter={4} figures={ch4Figures}/>
         <FigureGroup title='Chapter 8' chapter={8} figures={ch8Figures}/>
         <FigureGroup title='Chapter 9' chapter={9} figures={ch9Figures}/>
---
+++ src/chapters/ch2/Figures.js
@@ -0,0 +1,9 @@
+import Chart4 from './Chart4';
+
+export const figures = [
+  {
+    name: 'inset4',
+    figureClass: Chart4,
+    type: 'chart'
+  }
+];
