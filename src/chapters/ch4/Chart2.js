8175ed851029126d149fcb9577c69d07aa997785
Eric Brelsford <ebrelsford@gmail.com>
2017-11-08 11:54:57 -0500

    Chapter 4, inset 2

---
+++ src/chapters/ch4/Chart2.js
@@ -0,0 +1,95 @@
+import { extent } from 'd3-array';
+import { nest } from 'd3-collection';
+import { csv } from 'd3-request';
+import { scaleLinear, scaleOrdinal, scaleTime } from 'd3-scale';
+import { timeParse } from 'd3-time-format';
+
+import { schemeCategoryProblem } from '../../colors';
+import LineChart from '../../charts/LineChart';
+
+export default class Chart2 extends LineChart {
+  constructor(parent, width, height) {
+    super(parent, width, height);
+    this.xLabel = 'Year';
+    this.yLabel = 'Smoking Prevalence (%)';
+    this.legendItems = [
+      { label: 'Niger', value: 'Niger' },
+      { label: 'Congo', value: 'Congo' },
+      { label: 'Lesotho', value: 'Lesotho' },
+      { label: 'Mauritania', value: 'Mauritania' },
+    ];
+  }
+
+  createMargin() {
+    const margin = super.createMargin();
+    margin.top = 10;
+    margin.right = 10;
+    margin.bottom = this.legendOrientation() === 'horizontal' ? 32 : 58;
+    return margin;
+  }
+
+  loadData() {
+    return new Promise((resolve, reject) => {
+      csv('data/4-2.csv', (csvData) => {
+        const mappedData = csvData
+          .map(row => {
+            row.year = timeParse('%Y')(row.year);
+            row.value = +row.value;
+            return row;
+          });
+        this.flatData = mappedData;
+
+        const nestedData = nest()
+          .key(d => d.country)
+          .entries(mappedData);
+        resolve(nestedData);
+      });
+    });
+  }
+
+  lineXAccessor(d) {
+    return this.x(d.year);
+  }
+
+  lineYAccessor(d) {
+    return this.y(d.value);
+  }
+
+  createXScale() {
+    return scaleTime()
+      .range([0, this.chartWidth])
+      .domain(extent(this.flatData, d => d.year));
+  }
+
+  createYScale() {
+    let yExtent = extent(this.flatData, d => d.value);
+    yExtent[0] = Math.min(0, yExtent[0]);
+    return scaleLinear()
+      .range([this.chartHeight, 0])
+      .domain(yExtent);
+  }
+
+  createZScale() {
+    return scaleOrdinal(schemeCategoryProblem);
+  }
+
+  renderCircles() {
+    const circleGroups = this.root.selectAll('.circle')
+      .data(this.flatData)
+      .enter().append('g');
+
+    circleGroups.append('circle')
+      .attr('cx', d => this.x(d.year))
+      .attr('cy', d => this.y(d.value))
+      .attr('r', 1.5);
+  }
+
+  render() {
+    super.render();
+    this.renderCircles();
+
+    let xAxisHeight = this.parent.select('.axis-x .tick').node().getBBox().height + 18;
+    this.parent.select('.axis-x .axis-title')
+      .attr('transform', `translate(${this.chartWidth / 2}, ${xAxisHeight})`);
+  }
+}
--- src/chapters/ch4/Figures.js
+++ src/chapters/ch4/Figures.js
@@ -1,4 +1,5 @@
 import { Chart1 } from './Chart1';
+import Chart2 from './Chart2';
 import { Chart3 } from './Chart3';
 import { Chart4 } from './Chart4';
 import Map from './Map';
@@ -12,6 +13,11 @@ export const figures = [
   {
     name: 'inset1',
     figureClass: Chart1,
+    type: 'chart'
+  },
+  {
+    name: 'inset2',
+    figureClass: Chart2,
     type: 'chart'
   },
   {
