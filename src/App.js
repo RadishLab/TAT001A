a09ff3f3edbc5e78f29d82dfee59f3c3728068d4
Eric Brelsford <ebrelsford@gmail.com>
2017-11-09 14:17:19 -0500

    Chapter 7 map

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { figures as ch3Figures } from './chapters/ch3/Figures';
 import { figures as ch4Figures } from './chapters/ch4/Figures';
 import { figures as ch5Figures } from './chapters/ch5/Figures';
 import { figures as ch6Figures } from './chapters/ch6/Figures';
+import { figures as ch7Figures } from './chapters/ch7/Figures';
 import { figures as ch8Figures } from './chapters/ch8/Figures';
 import { figures as ch9Figures } from './chapters/ch9/Figures';
 import { figures as ch10Figures } from './chapters/ch10/Figures';
@@ -26,6 +27,7 @@ class App extends Component {
         <FigureGroup title='Chapter 4' chapter={4} figures={ch4Figures}/>
         <FigureGroup title='Chapter 5' chapter={5} figures={ch5Figures}/>
         <FigureGroup title='Chapter 6' chapter={6} figures={ch6Figures}/>
+        <FigureGroup title='Chapter 7' chapter={7} figures={ch7Figures}/>
         <FigureGroup title='Chapter 8' chapter={8} figures={ch8Figures}/>
         <FigureGroup title='Chapter 9' chapter={9} figures={ch9Figures}/>
         <FigureGroup title='Chapter 10' chapter={10} figures={ch10Figures}/>
---
+++ src/chapters/ch7/Map.js
@@ -0,0 +1,37 @@
+import { set } from 'd3-collection';
+import { csv } from 'd3-request';
+import { scaleOrdinal } from 'd3-scale';
+
+import { schemeCategoryProblemMap } from '../../colors';
+import { dataUrl } from '../../dataService';
+import WorldMap from '../../maps/WorldMap';
+
+export default class Map extends WorldMap {
+  constructor(parent, width, height) {
+    super(parent, width, height);
+    this.colorScale = scaleOrdinal(schemeCategoryProblemMap);
+    this.colorScaleType = 'ordinal';
+    this.valueField = 'Key Code';
+  }
+
+  loadJoinData() {
+    return new Promise((resolve, reject) => {
+      csv(dataUrl('7-map.csv'), (csvData) => {
+        const filteredData = csvData.filter(d => d[this.valueField]);
+        const domain = set(filteredData.map(d => d[this.valueField])).values().sort();
+        this.colorScale.domain(domain);
+        resolve(filteredData);
+      });
+    });
+  }
+
+  join(countries, joinData) {
+    countries.features.forEach(feature => {
+      const countryData = joinData.filter(row => row.ISO3 === feature.properties.iso_a3);
+      if (countryData.length > 0) {
+        feature.properties.joined = countryData[0];
+      }
+    });
+    return countries;
+  }
+}
