9b6a27155d6a867651468f3039b6bdf79c16255a
Eric Brelsford <ebrelsford@gmail.com>
2017-11-09 18:56:12 -0500

    Chapter 10 chart 3

---
+++ src/chapters/ch10/Chart3.js
@@ -0,0 +1,73 @@
+import { max, min } from 'd3-array';
+import { csv } from 'd3-request';
+import { scaleLinear, scaleOrdinal, scaleBand } from 'd3-scale';
+
+import { schemeCategorySolution } from '../../colors';
+import BarChart from '../../charts/BarChart';
+
+export default class Chart3 extends BarChart {
+  constructor(parent, width, height) {
+    super(parent, width, height);
+    this.yLabel = 'Revenue growth (in Million 2014 PPP dollars)';
+    this.yTicks = 6;
+    this.legendItems = [];
+  }
+
+  loadData() {
+    return new Promise((resolve, reject) => {
+      csv('data/10-3.csv', (csvData) => {
+        resolve(csvData.map(d => {
+          d.value = +d['Revenue growth (in Million 2014 PPP dollars)'];
+          d.region = d['Annual Excise Revenue (Million International Dollars PPP)'];
+          return d;
+        }));
+      });
+    });
+  }
+
+  createMargin() {
+    const margin = super.createMargin();
+    margin.bottom = this.legendOrientation() === 'horizontal' ? 30 : 30;
+    return margin;
+  }
+
+  createXScale() {
+    const values = this.data.map(this.getXValue);
+    return scaleBand()
+      .range([0, this.chartWidth])
+      .padding(0.5)
+      .domain(values);
+  }
+
+  createYScale() {
+    const values = this.data.map(d => d.value);
+    const yExtent = [
+      min(values.concat(0)),
+      max(values)
+    ];
+    return scaleLinear()
+      .range([this.chartHeight, 0])
+      .domain(yExtent);
+  }
+
+  getXValue(d) {
+    return d.region;
+  }
+
+  renderBars() {
+    const barGroups = this.createBarGroups();
+    const barWidth = this.x.bandwidth();
+
+    barGroups.append('rect')
+        .classed('bar', true)
+        .attr('x', d => this.x(this.getXValue(d)))
+        .attr('width', barWidth)
+        .attr('y', d => this.y(d.value))
+        .attr('height', d => this.chartHeight - this.y(d.value))
+        .attr('fill', this.colors(1));
+  }
+
+  createZScale() {
+    return scaleOrdinal(schemeCategorySolution);
+  }
+}
