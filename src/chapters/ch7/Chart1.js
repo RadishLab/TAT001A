4a48e4521136ad25bcbb73f2aefb03674dc5ae5a
Eric Brelsford <ebrelsford@gmail.com>
2017-11-09 15:43:06 -0500

    Chart 1

---
+++ src/chapters/ch7/Chart1.js
@@ -0,0 +1,83 @@
+import { extent } from 'd3-array';
+import { nest } from 'd3-collection';
+import { csv } from 'd3-request';
+import { scaleLinear, scaleOrdinal, scaleTime } from 'd3-scale';
+import { curveBasis, line } from 'd3-shape';
+import { timeParse } from 'd3-time-format';
+
+import { schemeCategoryProblem } from '../../colors';
+import LineChart from '../../charts/LineChart';
+
+export default class Chart1 extends LineChart {
+  constructor(parent, width, height) {
+    super(parent, width, height);
+    this.xLabel = 'Year';
+    this.yLabel = 'Tuberculosis mortality per 10k';
+    this.legendItems = [
+      { label: 'Baseline scenario', value: 'including smoking' },
+      { label: 'Without smoking', value: 'not including smoking' }
+    ];
+  }
+
+  loadData() {
+    return new Promise((resolve, reject) => {
+      csv('data/7-1.csv', (csvData) => {
+        const mappedData = csvData
+          .map(row => {
+            row.year = timeParse('%Y')(row.year);
+            row.value = +row['tuberculosis mortality per 10,000'];
+            return row;
+          });
+        const nestedData = nest()
+          .key(d => d.scenario)
+          .entries(mappedData);
+        resolve(nestedData);
+      });
+    });
+  }
+
+  onDataLoaded(data) {
+    this.x = this.createXScale();
+    this.y = this.createYScale();
+    this.colors = this.createZScale();
+    this.line = line()
+      .curve(curveBasis)
+      .x(this.lineXAccessor.bind(this))
+      .y(this.lineYAccessor.bind(this));
+    this.render();
+  }
+
+  createMargin() {
+    const margin = super.createMargin();
+    margin.bottom = this.legendOrientation() === 'horizontal' ? 45 : 60;
+    return margin;
+  }
+
+  lineXAccessor(d) {
+    return this.x(d.year);
+  }
+
+  lineYAccessor(d) {
+    return this.y(d.value);
+  }
+
+  createXScale() {
+    const values = this.data.reduce((valueArray, value) => valueArray.concat(value.values), []);
+    return scaleTime()
+      .range([0, this.chartWidth])
+      .domain(extent(values, d => d.year));
+  }
+
+  createYScale() {
+    const values = this.data.reduce((valueArray, value) => valueArray.concat(value.values), []);
+    let yExtent = extent(values, d => d.value);
+    yExtent[0] = Math.min(0, yExtent[0]);
+    return scaleLinear()
+      .range([this.chartHeight, 0])
+      .domain(yExtent);
+  }
+
+  createZScale() {
+    return scaleOrdinal(schemeCategoryProblem);
+  }
+}
