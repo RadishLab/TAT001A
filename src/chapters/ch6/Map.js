94bb8293488836a0f36b3f5ab311a0b78bec4a26
Eric Brelsford <ebrelsford@gmail.com>
2018-02-12 17:54:56 -0500

    Chapter 6 map tooltips

--- src/chapters/ch6/Map.js
+++ src/chapters/ch6/Map.js
@@ -1,3 +1,4 @@
+import { format } from 'd3-format';
 import { csv } from 'd3-request';
 import { scaleLinear } from 'd3-scale';
 
@@ -32,5 +33,17 @@ export default class Map extends WorldMap {
       }
     });
     return countries;
+  }
+
+  tooltipContent(d) {
+    let content = `<div class="country-name">${d.properties.NAME}</div>`;
+    const percentFormat = d => format('.1f')(d * 100);
+    if (d.properties.joined) {
+      content += `<div class="data">${percentFormat(d.properties.joined[this.valueField])}% ${this.getTranslation('of DALYs attributable to tobacco use')}</div>`;
+    }
+    else {
+      content += `<div class="data no-data">${this.getTranslation('No data')}</div>`;
+    }
+    return content;
   }
 }
