530dc0a0df8624c94e43213bfe6ccaece886d199
Eric Brelsford <ebrelsford@gmail.com>
2018-02-17 13:36:54 -0500

    Tooltip 12-map

--- src/chapters/ch12/Map.js
+++ src/chapters/ch12/Map.js
@@ -1,4 +1,5 @@
 import { set } from 'd3-collection';
+import { format } from 'd3-format';
 import { csv } from 'd3-request';
 import { scaleOrdinal } from 'd3-scale';
 
@@ -19,6 +20,7 @@ export default class Map extends WorldMap {
       csv(this.dataFileUrl('12-map.csv'), (csvData) => {
         const mappedData = csvData.map(d => {
           d[this.symbolField] = d[this.symbolField] === '1';
+          d.price = +d['PRICE OF A 20-CIGARETTE PACK OF THE MOST-SOLD BRAND, US DOLLARS (adjusted for purchasing power of national currencies)'];
           return d;
         });
         const filteredData = mappedData.filter(d => d[this.valueField] !== '');
@@ -37,5 +39,17 @@ export default class Map extends WorldMap {
       }
     });
     return countries;
+  }
+
+  tooltipContent(d) {
+    let content = `<div class="country-name">${d.properties.NAME}</div>`;
+    const currencyFormat = format('.2f');
+    if (d.properties.joined) {
+      content += `<div class="data">${this.getTranslation('Price of 20-cigarette pack of the most-sold brand (USD)')}: $${currencyFormat(d.properties.joined.price)}</div>`;
+    }
+    else {
+      content += `<div class="data no-data">${this.getTranslation('No data')}</div>`;
+    }
+    return content;
   }
 }
