15c3b74df084989932fc772b6a3d0b275ccf8d8d
Eric Brelsford <ebrelsford@gmail.com>
2018-02-21 18:13:17 -0500

    Never break Viet Nam

--- src/wrap.js
+++ src/wrap.js
@@ -3,14 +3,14 @@ import { select } from 'd3-selection';
 export default function wrap(text, width) {
   text.each(function() {
     var text = select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word = words.pop(),
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.1, // ems
-        y = text.attr('y'),
-        dy = parseFloat(text.attr('dy')),
-        tspan = text.text(null).append('tspan').attr('x', 0).attr('y', y).attr('dy', dy + 'em');
+      words = text.text() === 'Viet Nam' ? ['Viet Nam'] : text.text().split(/\s+/).reverse(),
+      word = words.pop(),
+      line = [],
+      lineNumber = 0,
+      lineHeight = 1.1, // ems
+      y = text.attr('y'),
+      dy = parseFloat(text.attr('dy')),
+      tspan = text.text(null).append('tspan').attr('x', 0).attr('y', y).attr('dy', dy + 'em');
     while (word) {
       line.push(word);
       tspan.text(line.join(' '));
