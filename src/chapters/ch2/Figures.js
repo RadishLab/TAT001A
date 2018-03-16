c4d7902e23530bfd9dc0ac8e6852e39ed36f736e
Eric Brelsford <ebrelsford@gmail.com>
2018-03-16 10:31:22 -0400

    2-map2

--- src/chapters/ch2/Figures.js
+++ src/chapters/ch2/Figures.js
@@ -1,10 +1,16 @@
 import Chart4 from './Chart4';
 import Map from './Map';
+import Map2 from './Map2';
 
 export const figures = [
   {
     name: 'map',
     figureClass: Map,
+    type: 'map'
+  },
+  {
+    name: 'map2',
+    figureClass: Map2,
     type: 'map'
   },
   {
---
+++ src/chapters/ch2/Map2.js
@@ -0,0 +1,50 @@
+import { set } from 'd3-collection';
+import { csv } from 'd3-request';
+import { scaleOrdinal } from 'd3-scale';
+
+import { schemeCategoryProblem } from '../../colors';
+import WorldMap from '../../maps/WorldMap';
+
+export default class Map2 extends WorldMap {
+  constructor(parent, options) {
+    super(parent, options);
+    this.colorScale = scaleOrdinal(schemeCategoryProblem);
+    this.colorScaleType = 'ordinal';
+    this.valueField = 'Key';
+  }
+
+  loadJoinData() {
+    return new Promise((resolve, reject) => {
+      csv(this.dataFileUrl('2-map2.csv'), (csvData) => {
+        const filteredData = csvData.filter(d => d[this.valueField] !== '');
+        const domain = set(filteredData.map(d => d[this.valueField])).values().sort();
+        this.colorScale.domain(domain);
+        resolve(filteredData);
+      });
+    });
+  }
+
+  join(countries, joinData) {
+    countries.features.forEach(feature => {
+      const countryData = joinData.filter(row => row['ISO3 CODE'] === this.getISO3(feature));
+      if (countryData.length > 0) {
+        feature.properties.joined = countryData[0];
+      }
+    });
+    return countries;
+  }
+
+  tooltipContent(d) {
+    let content = `<div class="country-name">${d.properties.NAME}</div>`;
+    if (d.properties.joined) {
+      const marketLeader = d.properties.joined['Market Leader (Company Name)'];
+      if (marketLeader) {
+        content += `<div class="data">${this.getTranslation('Market leader')}: ${marketLeader}</div>`;
+      }
+    }
+    else {
+      content += `<div class="data no-data">${this.getTranslation('No data')}</div>`;
+    }
+    return content;
+  }
+}
