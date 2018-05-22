151d129e6f9ec59e4c5bf106dd750a972562c198
Eric Brelsford <ebrelsford@gmail.com>
2018-05-22 19:16:55 -0400

    Add category map

---
+++ src/usergenerated/UserGeneratedCategoryMap.js
@@ -0,0 +1,83 @@
+import { set } from 'd3-collection';
+import { csv, json as d3json } from 'd3-request';
+import { scaleOrdinal } from 'd3-scale';
+import * as topojson from 'topojson-client';
+
+import { schemeCategoryProblem, schemeCategorySolution } from '../colors';
+import WorldMap from '../maps/WorldMap';
+
+export default class UserGeneratedCategoryMap extends WorldMap {
+  constructor(parent, options) {
+    super(parent, options);
+    this.options = options;
+
+    this.colorScheme = this.options.colorScheme || 'problems';
+    this.iso3Column = this.options.iso3Column;
+    this.symbolField = this.options.symbolColumn;
+    this.valueField = this.options.categoryColumn;
+    this.tooltipColumn = this.options.tooltipColumn;
+  }
+
+  createColorScale(domain) {
+    let scale;
+    const scheme = this.colorScheme === 'problems'
+      ? schemeCategoryProblem
+      : schemeCategorySolution;
+    scale = scaleOrdinal(scheme)
+      .domain(domain);
+    return scale;
+  }
+
+  loadCountries() {
+    return new Promise((resolve, reject) => {
+      d3json(this.options.countriesTopojsonUrl, (data) => {
+        resolve(topojson.feature(data, data.objects['-']));
+      });
+    });
+  }
+
+  loadJoinData() {
+    return new Promise((resolve, reject) => {
+      csv(this.options.dataUrl, (csvData) => {
+        csvData = csvData.map(d => {
+          if (this.symbolField) {
+            d[this.symbolField] = d[this.symbolField] && d[this.symbolField].toLowerCase() === 'yes';
+          }
+          return d;
+        });
+        this.domain = set(csvData.map(d => d[this.valueField])).values().sort();
+        this.colorScale = this.createColorScale(this.domain);
+        resolve(csvData);
+      });
+    });
+  }
+
+  join(countries, joinData) {
+    countries.features.forEach(feature => {
+      const countryData = joinData
+        .filter(row => row[this.iso3Column] === this.getISO3(feature));
+      if (countryData.length > 0) {
+        feature.properties.joined = countryData[0];
+      }
+    });
+    return countries;
+  }
+
+  countryFill(d) {
+    if (d.properties.joined) {
+      const value = d.properties.joined[this.valueField];
+      if (value && value !== '') return this.colorScale(value);
+    }
+    return this.noDataColor;
+  }
+
+  tooltipContent(d) {
+    let content = `<div class="country-name">${d.properties.NAME}</div>`;
+    let details;
+    if (this.tooltipColumn && d.properties.joined) {
+      details = d.properties.joined[this.tooltipColumn];
+    }
+    content += `<div>${details ? details : this.getTranslation('No data')}</div>`;
+    return content;
+  }
+}
--- src/usergenerated/UserGeneratedFigure.js
+++ src/usergenerated/UserGeneratedFigure.js
@@ -1,12 +1,14 @@
 import { select } from 'd3-selection';
 
 import UserGeneratedBarChart from './UserGeneratedBarChart';
+import UserGeneratedCategoryMap from './UserGeneratedCategoryMap';
 import UserGeneratedChoroplethMap from './UserGeneratedChoroplethMap';
 import UserGeneratedLineChart from './UserGeneratedLineChart';
 import UserGeneratedPointMap from './UserGeneratedPointMap';
 
 const visualizationTypes = {
   'bar chart': UserGeneratedBarChart,
+  'category map': UserGeneratedCategoryMap,
   'choropleth map': UserGeneratedChoroplethMap,
   'line chart': UserGeneratedLineChart,
   'point map': UserGeneratedPointMap,
