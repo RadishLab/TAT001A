bc934863250733d9c61a07470b6abada2c02f62a
Eric Brelsford <ebrelsford@gmail.com>
2020-04-09 12:14:29 -0400

    Add chapter 1 cartogram and treemap

--- src/chapters/ch1/Figures.js
+++ src/chapters/ch1/Figures.js
@@ -3,7 +3,9 @@ import Chart3 from './Chart3';
 import Chart4 from './Chart4';
 import Map from './Map';
 import Map2 from './Map2';
+import Map2Treemap from './Map2Treemap';
 import Map3 from './Map3';
+import Map3Cartogram from './Map3Cartogram';
 import Map4 from './Map4';
 
 export const figures = [
@@ -41,5 +43,15 @@ export const figures = [
     name: '4',
     figureClass: Chart4,
     type: 'chart'
+  },
+  {
+    name: 'map2treemap',
+    figureClass: Map2Treemap,
+    type: 'map'
+  },
+  {
+    name: 'map3cartogram',
+    figureClass: Map3Cartogram,
+    type: 'map'
   }
 ];
---
+++ src/chapters/ch1/Map2Treemap.js
@@ -0,0 +1,164 @@
+import { extent } from 'd3-array';
+import { set } from 'd3-collection';
+import { format } from 'd3-format';
+import { csv } from 'd3-request';
+import { scaleOrdinal } from 'd3-scale';
+import noUiSlider from 'nouislider';
+
+import { schemeCategoryProblemMap } from '../../colors';
+import TreeMap from '../../maps/TreeMap';
+
+/*
+ * Hectares planted with tobacco by country with year filters
+ */
+export default class Map2Treemap extends TreeMap {
+  constructor(parent, options) {
+    super(parent, options);
+    this.figurePrefix = '1-map2';
+    this.colorScale = scaleOrdinal(schemeCategoryProblemMap);
+    this.colorScaleType = 'ordinal';
+    this.valueField = 'Hectares - Keycode';
+
+    this.parentContainer.style('padding-top', '25px');
+
+    this.noDataColor = schemeCategoryProblemMap[0];
+  }
+
+  getLegendItems() {
+    return Object.entries(this.legend).reverse();
+  }
+
+  loadJoinData() {
+    return new Promise((resolve, reject) => {
+      csv(this.dataFileUrl('1-map2.csv'), (csvData) => {
+        const domain = set(csvData.map(d => d[this.valueField])).values().filter(d => d !== '').sort();
+        this.colorScale.domain(domain);
+        this.yearRange = extent(csvData, d => +d['Year']);
+        this.filterState = { year: this.yearRange[1] };
+        resolve(csvData);
+      });
+    });
+  }
+
+  join(countries, joinData) {
+    countries.features.forEach(feature => {
+      const countryData = joinData.filter(row => row.iso3code === this.getISO3(feature));
+      if (countryData.length > 0) {
+        feature.properties.joined = countryData.map(d => {
+          let value = parseInt(d['Area harvested'], 10);
+          if (isNaN(value)) value = null;
+          return {
+            keycode: d[this.valueField],
+            value,
+            year: +d['Year']
+          }
+        });
+      }
+    });
+    return countries;
+  }
+
+  countryFill(d) {
+    if (d.properties.joined) {
+      const matchingYears = d.properties.joined.filter(joinedData => joinedData.year === this.filterState.year);
+      if (matchingYears && matchingYears[0]) {
+        const keycode = matchingYears[0].keycode;
+        if (!keycode) return this.noDataColor;
+        return this.colorScale(keycode);
+      }
+    }
+    return this.noDataColor;
+  }
+
+  renderFilters() {
+    this.parentContainer.selectAll('.ta-visualization-filters')
+      .data([true])
+      .join(enter => {
+        if (this.filtersContainer) return;
+        // TODO move to CSS
+        this.filtersContainer = enter.append('div')
+          .classed('ta-visualization-filters', true)
+          .style('position', 'absolute')
+          .style('top', 25)
+          .style('left', 0)
+          .style('width', '100%')
+          .lower();
+
+        this.filtersContainer
+          .append('span')
+          .text(this.getTranslation('Filters:', null, 'Visualization'));
+
+        const yearInput = this.filtersContainer
+          .append('div').classed('filter-group', true)
+          .append('div')
+            .classed('filter-year', true);
+
+        const yearInputNode = yearInput.node();
+        noUiSlider.create(yearInputNode, {
+          start: this.yearRange[1],
+          range: {
+            min: this.yearRange[0],
+            max: this.yearRange[1]
+          },
+          step: 1,
+          tooltips: true,
+          format: {
+            to: d => d,
+            from: d => parseInt(d, 10)
+          },
+          pips: {
+            mode: 'steps',
+            filter: d => d % 10 === 0,
+            density: 5
+          }
+        });
+
+        yearInputNode.noUiSlider.on('update', (values) => {
+          this.updateFilters({ year: values[0] });
+        });
+
+        // On init select initial key
+        this.updateFilters(this.filterState);
+
+        return enter;
+      });
+  }
+
+  updateFilters(selectedFilter) {
+    this.filterState = { ...this.filterState, ...selectedFilter };
+    this.renderTreeSegments();
+  }
+
+  render() {
+    super.render();
+    this.renderFilters();
+  }
+
+  leafContent(d) {
+    /*
+    const parentContent = super.leafContent();
+    parentContent.append('tspan', d.data.value);
+    return parentContent;
+    */
+    const valueFormat = format(',d');
+    return valueFormat(d.data.value);
+  }
+
+  tooltipContent(d) {
+    let content = `<div class="country-name">${this.getCountryName(d)}</div>`;
+    const valueFormat = format(',d');
+    let value = d.data.value;
+
+    // Treat 0 different from null/undefined
+    if (value === 0) {
+      content += `<div class="data">${this.getTranslation('No reported production', null, '1-map2')}</div>`;
+    }
+    else if (!value) {
+      content += `<div class="data">${this.getTranslation('Data missing', null, '1-map2')}</div>`;
+    }
+    else {
+      content += `<div class="data">${this.getTranslation('Hectares of tobacco planted', null, '1-map2')}: ${valueFormat(value)}</div>`;
+    }
+    return content;
+  }
+}
